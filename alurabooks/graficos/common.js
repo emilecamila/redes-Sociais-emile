e539ff634ff8e592958056d82fb1a20cd1ba9b7a
emilecamila <128156770+emilecamila@users.noreply.github.com>
2024-09-24 12:16:40 -0300

    Add files via upload

---
+++ alurabooks/graficos/common.js
@@ -0,0 +1,12 @@
+</footer>
+<script type="module" src="graficos/common.js"></script>
+<script type="module" src="graficos/informacoesGlobais.js"></script>
+<script type="module" src="graficos/quantidadeUsuarios.js"></script>
+
+</body>
+</html>
+
+const getCSS = (variavel) => {
+    return getComputedStyle(document.body).getPropertyValue(variavel)
+  };
+  
